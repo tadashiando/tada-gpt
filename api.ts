fe04f18ec460bc2ee36d4e0707dc7eae38d67bbb
Tadashi Ando <ando.tadashi@gmail.com>
2025-01-10 20:05:10 -0300

    Now trying again

--- api.ts
+++ api.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import serverless from "serverless-http";
-import routes from "./src/routes/index.js";
+import cors from "cors";
+import routes from "./routes/index.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api", routes); // Prefix all routes with '/api'
 
@@ -14,4 +17,4 @@ app.use("/api", routes); // Prefix all routes with '/api'
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); */
 
-export const handler = serverless(api);
+export const handler = serverless(app);
