cfc7a07e79d71619c64ecb59043eb2c6f2567f31
Tadashi Ando <ando.tadashi@gmail.com>
2025-01-26 21:02:32 -0300

    Updated CORS configuration

--- server.ts
+++ server.ts
@@ -12,16 +12,20 @@ const whitelist = [
   "http://127.0.0.1:5173",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+  "https://tadagpt.onrender.com",
+  "https://tadagpt.netlify.app"
 ];
 
 const corsOptions = {
   origin: function (requestOrigin, callback) {
-    console.log(requestOrigin);
-    const isWhitelisted = whitelist.includes(requestOrigin);
-    if (isWhitelisted) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
+    if (requestOrigin) {
+      console.log(requestOrigin);
+      const isWhitelisted = whitelist.includes(requestOrigin);
+      if (isWhitelisted) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     }
   },
 };
